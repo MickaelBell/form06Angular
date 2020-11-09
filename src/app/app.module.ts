7490c11d2dfccb5d1acb69a963a40bf3d63609ff
mickaelbell <mickael.belleuvre@gmail.com>
2020-11-09 10:42:22 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { UserProfilComponent } from './user-profile/user-profil.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserProfilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
